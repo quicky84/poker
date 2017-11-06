dc0a044778459e5a550916cc5539c0529e97cdf7
Victor Emolaev <ve@ve-secman.local.nedap>
2017-11-06 19:55:31 +0100

    Use enums instead strings

--- src/poker/hand_test.ts
+++ src/poker/hand_test.ts
@@ -47,15 +47,15 @@ test("Optimize: 1 step, pick best between two possible under budget", t=> {
 
 test("Best hand computed correctly", t=>{
     const inputs = [
-        ["TH JH QC QD QS QH KH AH 2S 6S", "staright-flush"],
-        ["2H 2S 3H 3S 3C 2D 3D 6C 9C TH", "four-of-a-kind"],
-        ["2H 2S 3H 3S 3C 2D 9C 3D 6C TH", "full-house"],
-        ["2H AD 5H AC 7H AH 6H 9H 4H 3C", "flush"],
-        ["AC 2D 9C 3S KD 5S 4D KS AS 4C", "straight"],
-        ["KS AH 2H 3C 4H KC 2C TC 2D AS", "three-of-a-kind"],
-        ["AH 2C 9S AD 3C QH KS JS JD KD", "two-pairs"],
-        ["6C 9C 8C 2D 7C 2H TC 4C 9S AH", "one-pair"],
-        ["3D 5S 2H QD TD 6S KH 9H AD QH", "highest-card"],
+        ["TH JH QC QD QS QH KH AH 2S 6S", Combination.STRAIGHT_FLUSH],
+        ["2H 2S 3H 3S 3C 2D 3D 6C 9C TH", Combination.FOUR_OF_A_KIND],
+        ["2H 2S 3H 3S 3C 2D 9C 3D 6C TH", Combination.FULL_HOUSE],
+        ["2H AD 5H AC 7H AH 6H 9H 4H 3C", Combination.FLUSH],
+        ["AC 2D 9C 3S KD 5S 4D KS AS 4C", Combination.STRAIGHT],
+        ["KS AH 2H 3C 4H KC 2C TC 2D AS", Combination.THREE_OF_A_KIND],
+        ["AH 2C 9S AD 3C QH KS JS JD KD", Combination.TWO_PAIRS],
+        ["6C 9C 8C 2D 7C 2H TC 4C 9S AH", Combination.ONE_PAIR],
+        ["3D 5S 2H QD TD 6S KH 9H AD QH", Combination.HIGHEST_CARD],
     ];
 
     inputs.map(([input, best])=> {
